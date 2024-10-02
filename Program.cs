927dc90d8f198d886696eb118702ef2b5f64226b
larissa de oliveira <larissadeoliveira3zb@gmail.com>
2024-10-02 13:25:04 -0300

    JurosCompostos

--- Program.cs
+++ Program.cs
@@ -9,3 +9,4 @@ public class Program
     }
      public void Executar() //Ele executa as interações com o usuário, coletando entradas e exibindo os resultados no console
     {
+        Console.Write("Digite o capital (valor inicial): "); //Exibe uma mensagem no console pedindo ao usuário para inserir o valor do capital inicial.
