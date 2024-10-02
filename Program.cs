579251cdc47acb1bafe861afe005ceaf2fc42b21
larissa de oliveira <larissadeoliveira3zb@gmail.com>
2024-10-02 13:22:48 -0300

    JurosCompostos

--- Program.cs
+++ Program.cs
@@ -2,3 +2,7 @@
 
 public class Program
 {
+ public double CalcularJurosCompostos(double capital, double taxa, double tempoMeses) //Define um método público que retorna um valor double e recebe três parâmetros: capital, taxa e tempoMeses.
+
+    {
+        return capital * Math.Pow(1 + (taxa / 100) / 12, tempoMeses); //Calcula a potência de um número
