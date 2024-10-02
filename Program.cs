e3e3d0c1493bc7bd39e4fb884965eb9f5698d349
larissa de oliveira <larissadeoliveira3zb@gmail.com>
2024-10-02 13:28:45 -0300

    JurosCompostos

--- Program.cs
+++ Program.cs
@@ -11,3 +11,5 @@ public class Program
     {
         Console.Write("Digite o capital (valor inicial): "); //Exibe uma mensagem no console pedindo ao usuário para inserir o valor do capital inicial.
         double capital = double.Parse(Console.ReadLine()); //Converte a string digitada em um número do tipo double
+        Console.Write("Digite a taxa de juros anual (em %): "); //Exibe uma mensagem pedindo a taxa de juros anual, em porcentagem.
+        double taxa = double.Parse(Console.ReadLine());  //Converte a string digitada em um número do tipo double
